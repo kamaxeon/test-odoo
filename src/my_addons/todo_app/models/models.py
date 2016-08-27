ed1774b509b198a73cc629ac2106385e987294e5
Israel Santana <isra@miscorreos.org>
2016-08-27 12:13:48 +0100

    Add from to create tasks

--- src/my_addons/todo_app/models/models.py
+++ src/my_addons/todo_app/models/models.py
@@ -2,6 +2,13 @@
 
 from openerp import models, fields, api
 
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
+
+    name = fields.Char(string="Description", required=True, )
+    is_done = fields.Boolean(string="Done?",  )
+    active = fields.Boolean(string="Active?", default=True )
 # class todo_app(models.Model):
 #     _name = 'todo_app.todo_app'
 
