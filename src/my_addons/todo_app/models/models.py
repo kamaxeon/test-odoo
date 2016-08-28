ee43c16a1fd3b05587cdfa66e38d5fbcd50fc989
Israel Santana <isra@miscorreos.org>
2016-08-28 11:54:05 +0100

    Add logic to button

--- src/my_addons/todo_app/models/models.py
+++ src/my_addons/todo_app/models/models.py
@@ -9,6 +9,17 @@ class TodoTask(models.Model):
     name = fields.Char(string="Description", required=True, )
     is_done = fields.Boolean(string="Done?",  )
     active = fields.Boolean(string="Active?", default=True )
+
+    @api.one
+    def do_toogle_done(self):
+        self.is_done = not self.is_done
+        return True
+
+    @api.multi
+    def do_clear_done(self):
+        done_recs = self.search([('is_done', '=', True)])
+        done_recs.write({'active': False})
+        return True
 # class todo_app(models.Model):
 #     _name = 'todo_app.todo_app'
 
